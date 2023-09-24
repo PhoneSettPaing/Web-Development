8549236927add69563ee9420ea6f78fc13c8416d
Phone Sett Paing <102354002+PhoneSettPaing@users.noreply.github.com>
2023-09-24 20:51:11 +0630

    Add files via upload

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require("sillyname");
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+var superheroesName = superheroes.random();
+
+console.log(`I am ${superheroesName}!`);
