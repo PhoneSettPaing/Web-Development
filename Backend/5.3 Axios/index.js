6119a8aaa8df40d26ad7f641c01504bf92cccdfe
Phone Sett Paing <102354002+PhoneSettPaing@users.noreply.github.com>
2023-10-16 20:28:23 +0630

    Add files via upload

---
+++ Backend/5.3 Axios/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Step 1: Make sure that when a user visits the home page,
+//   it shows a random activity.You will need to check the format of the
+//   JSON data from response.data and edit the index.ejs file accordingly.
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+    //console.log(result);
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  //console.log(req.body);
+  
+  // Step 2: Play around with the drop downs and see what gets logged.
+  // Use axios to make an API request to the /filter endpoint. Making
+  // sure you're passing both the type and participants queries.
+  // Render the index.ejs file with a single *random* activity that comes back
+  // from the API request.
+  // Step 3: If you get a 404 error (resource not found) from the API request.
+  // Pass an error to the index.ejs to tell the user:
+  // "No activities that match your criteria."
+
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/filter", { params: req.body });
+    const result = response.data;
+    const randNum = Math.floor(Math.random() * result.length);
+    res.render("index.ejs", { data: result[randNum]});
+    //console.log(result[randNum]);
+  } catch (error) {
+    console.error("No activities that match your criteria.",error.message);
+    res.render("index.ejs", { error: "No activities that match your criteria." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
