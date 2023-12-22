3ddf6b4bef39fb2cd83ddfccfc1b91aa9af5676c
Phone Sett Paing <102354002+PhoneSettPaing@users.noreply.github.com>
2023-12-22 23:34:24 +0630

    Add files via upload

---
+++ React/16. react-devtools-forked/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card.jsx";
+import contacts from "../contacts.js";
+import Avatar from "./Avatar.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://miro.medium.com/v2/resize:fit:2400/1*8OkdLpw_7VokmSrzwXLnbg.jpeg" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
